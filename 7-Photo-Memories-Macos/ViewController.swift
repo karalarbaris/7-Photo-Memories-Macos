7959ba7c4eb4f3043c09716be7567cc713e9e869
Baris <karalarbaris@gmail.com>
2022-01-04 11:59:29 +0300

    Initial Commit

---
+++ 7-Photo-Memories-Macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  7-Photo-Memories-Macos
+//
+//  Created by Baris Karalar on 4.01.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
