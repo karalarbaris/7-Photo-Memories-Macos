9e5e9cf0b693f8a9ebdc16c7a82ff242d6a7bba3
Baris <karalarbaris@gmail.com>
2022-01-05 20:28:38 +0300

    Loading images from disk

--- 7-Photo-Memories-Macos/ViewController.swift
+++ 7-Photo-Memories-Macos/ViewController.swift
@@ -8,35 +8,65 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet var collectionView: NSCollectionView!
+    
+    lazy var photosDirectory: URL = {
+        let fm = FileManager.default
+        let paths = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        let saveDirectory = documentsDirectory.appendingPathComponent("slidesforp7", isDirectory: true)
+        
+        if !fm.fileExists(atPath: saveDirectory.path) {
+            try? fm.createDirectory(at: saveDirectory, withIntermediateDirectories: true)
+        }
+            
+            return saveDirectory
+    }()
+    
+    var photos = [URL]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        do {
+            let fm = FileManager.default
+            let files = try fm.contentsOfDirectory(at: photosDirectory, includingPropertiesForKeys: nil)
+            
+            for file in files {
+                if file.pathExtension == "jpg" || file.pathExtension == "png" {
+                    photos.append(file)
+                }
+            }
+        } catch  {
+            // failed to read the save directory
+            print("Set up error")
+        }
+        
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
 
 extension ViewController: NSCollectionViewDataSource, NSCollectionViewDelegate {
     
     func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return photos.count
     }
     
     func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
         let item = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier("Photo"), for: indexPath)
         guard let pictureItem = item as? Photo else { return item }
         
-        pictureItem.view.layer?.backgroundColor = NSColor.red.cgColor
+        let img = NSImage(contentsOf: photos[indexPath.item])
+        pictureItem.imageView?.image = img
         
         return pictureItem
     }
